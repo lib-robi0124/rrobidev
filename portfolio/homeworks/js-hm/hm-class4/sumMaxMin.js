ba111a309a9c3dd19a55844d594105e79521d77e
lib-robi0124 <robert.ristovski@gmail.com>
2024-12-17 15:09:30 +0100

    sdf

--- portfolio/homeworks/js-hm/hm-class4/sumMaxMin.js
+++ portfolio/homeworks/js-hm/hm-class4/sumMaxMin.js
@@ -5,15 +5,12 @@ let sumMaxMin = 0;
       for (let i = 1; i < numberArray.length; i++) {
           if (maxItem < numberArray[i]) {
               maxItem = numberArray[i];
-          }
+          } else if (minItem > numberArray[i]) {
+            minItem = numberArray[i];
+         }
         }
         console.log(`Biggest number in array is ${maxItem}`);
      
-        for (let i = 1; i < numberArray.length; i++) {
-        if (minItem > numberArray[i]) {
-            minItem = numberArray[i];
-         }
-       }
         console.log(`Smallest number in array is ${minItem}`);
 sumMaxMin = maxItem + minItem;
 console.log(`Sum of Max i Min is ${sumMaxMin}`);
