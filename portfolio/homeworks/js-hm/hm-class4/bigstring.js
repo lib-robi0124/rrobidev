a160fdc2cead1f2e410c5c4305344cc0f0068531
lib-robi0124 <robert.ristovski@gmail.com>
2024-12-17 10:01:52 +0100

    asd

--- portfolio/homeworks/js-hm/hm-class4/bigstring.js
+++ portfolio/homeworks/js-hm/hm-class4/bigstring.js
@@ -1,22 +1,3 @@
+// Hello there
 let arrayOfStrings = ["Robert", "Hello", "Qinshift", "there", "students", "play", "of", "SEDC"];
-let bigString = [];
-
-for (let i = 0; i < arrayOfStrings.length; i++) {
-  if (arrayOfStrings[i] === "Hello") {
-  bigString.push(arrayOfStrings[i]);
-      continue;
-  } else  if (arrayOfStrings[i] === "there") {
-    bigString.push(arrayOfStrings[i]);
-        continue;
-  } else  if (arrayOfStrings[i] === "students") {
-    bigString.push(arrayOfStrings[i]);
-        continue;
-  } else  if (arrayOfStrings[i] === "of") {
-    bigString.push(arrayOfStrings[i]);
-        continue;
-  } else  if (arrayOfStrings[i] === "SEDC") {
-    bigString.push(arrayOfStrings[i]);
-        continue;
-  }
-} 
-console.log(bigString);
+alert(`${arrayOfStrings[2]} ${" "} ${arrayOfStrings[3]} ${" "} ${arrayOfStrings[4]} ${" "} ${arrayOfStrings[6]} ${" "} ${arrayOfStrings[7]} !!`);
