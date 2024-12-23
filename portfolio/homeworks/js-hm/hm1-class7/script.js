e4bf75f9b0cbb9ed0439d18e69580cf1bb1c8818
lib-robi0124 <robert.ristovski@gmail.com>
2024-12-23 14:35:20 +0100

    asdf

---
+++ portfolio/homeworks/js-hm/hm1-class7/script.js
@@ -0,0 +1,11 @@
+let firstDiv = document.getElementById("first");
+let firstDivChild = firstDiv.firstElementChild;
+firstDivChild.innerText = "Best web page!";
+console.log(firstDivChild);
+let paragraphs = document.getElementsByTagName('p');
+paragraphs[0].innerHTML = "This is game, find me now";
+paragraphs[1].innerHTML = "Copy paste is not working always";
+console.log(paragraphs);
+let lastDiv = document.getElementsByTagName('h1');
+lastDiv[1].innerHTML = "I found you to be change it";
+console.log(lastDiv);
