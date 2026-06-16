b88302d95a5378597e27912e4a6f0ffb71ff5878
lib-robi0124 <robert.ristovski@libertysteelgroup.com>
2026-06-16 11:43:16 +0200

    brisi

--- portfolio/homeworks/js-hm/hm-typeof/script.js
+++
@@ -1,9 +0,0 @@
-function printtypeof(value){
-    let typeofvalue = value;
-    return typeofvalue;
-}
-console.log(printtypeof(typeof(" ")));
-console.log(printtypeof(typeof(10)));
-console.log(printtypeof(typeof(Null)));
-console.log(printtypeof(typeof(5 == 6)));
-
